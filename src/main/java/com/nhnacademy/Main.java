7553f1be586f3d7b020b8b09fe6b5c1c31d063a5
eugene <hash_ff0000@naver.com>
2025-10-03 13:52:52 +0900

    1000

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -1,10 +1,17 @@
 package com.nhnacademy;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        // ---> nextInt() 는 공백을 기준으로 토큰을 읽어옴
+        // 그래서 '1 2(엔터)' 이렇게 입력해도 각각 변수에 대입 가능
+        // 근데 그 공백이나 구분자는 버퍼에 남겨놓고 숫자만 갖고온다는거 주의하셈
+        System.out.println(A+B);
+
     }
 }
