e4f7f2c91ba5ef544accd97bb6ff93c5996f6707
eugene <hash_ff0000@naver.com>
2025-10-09 12:16:42 +0900

    8958

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -6,18 +6,25 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int H = sc.nextInt();
-        int M = sc.nextInt();
-
-        M += H * 60; // 분으로 환산후 M에 합산
-        M -= 45; // 45분 빼기
-        if(M < 0) { // 만약 날짜가 넘어간다면?
-            M = 1440 + M; // M이 음수니까 더하는게 맞음
+        int T = sc.nextInt();
+//        sc.next();
+        int bonus = 0;
+        int score = 0;
+        String str;
+        for(int j=0; j<T; j++) {
+            str = sc.next();
+            for(int i=0; i<str.length(); i++) {
+                char ch = str.charAt(i);
+                if(ch == 'O') {
+                    score++;
+                    score += bonus++;
+                } else if(ch == 'X') {
+                    bonus = 0;
+                }
+            }
+            System.out.println(score);
+            score=0;
+            bonus=0;
         }
-
-        H = M/60;
-        M %= 60;
-
-        System.out.printf("%d %d\n", H, M);
     }
 }
